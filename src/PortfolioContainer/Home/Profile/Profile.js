fbe4dbcb0f99016c1ded981d1568d37f3c640fce
Michael Fernandez <mikefernandezgm@gmail.com>
2022-01-04 23:26:49 -0500

    my Portfolio

---
+++ src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,74 @@
+import React from "react";
+import Typical from "react-typical";
+import ScrollService from "../../../utilities/ScrollService";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/Mik3fernand/" target="_blank" rel="noopener noreferrer" >
+                <i className="fa fa-facebook-square" />
+              </a>
+              <a href="https://www.instagram.com/mikefernand35/" target="_blank" rel="noopener noreferrer" >
+                <i className="fa fa-instagram" />
+              </a>
+              <a href="https://twitter.com/Michael04859420" target="_blank" rel="noopener noreferrer" >
+                <i className="fa fa-twitter" />
+              </a>
+              <a href="https://github.com/fmik3r" target="_blank" rel="noopener noreferrer" >
+                <i className="fa fa-github-square" />
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">Mike</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 😎",
+                    1000,
+                    "Full stack Developer 💻",
+                    1000,
+                    "Mern stack Dev 📱",
+                    1000,
+                    "Cross Platform Dev 🔴",
+                    1000,
+                    "React/React Native 🌐",
+                    1000,
+                  ]}
+                />
+              </h1>
+            </span>
+            <span className="profile-role-tagline">
+              Knack of building applications with front and back end operations.
+            </span>
+          </div>
+
+          <div className="profile-options">
+            <button className="btn primary-btn"
+            onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+            > Hire Me </button>
+            <a href="Mykcv.pdf" download="Mykcv.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
